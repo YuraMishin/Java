dcf8ff7259a8d4d1a808d2f482132583760a58bd
Mishin Yura <mishin.inbox@gmail.com>
2021-08-24 15:59:11 +0300

    Added Unit Test to check if DB scheme is successfully created. Closes #33

--- university/src/test/java/com/mishinyura/university/StartAppTests.java
+++ university/src/test/java/com/mishinyura/university/StartAppTests.java
@@ -1,7 +1,15 @@
 package com.mishinyura.university;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Class StartAppTests.
@@ -11,12 +19,36 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @since 24.08.2021
  */
 @SpringBootTest
+@ActiveProfiles("test")
 class StartAppTests {
     /**
-     * Tests smth.
+     * JdbcTemplate.
+     */
+    @Autowired
+    JdbcTemplate template;
+
+    /**
+     * Tests DB Schema.
      */
     @Test
-    void shouldTest() {
+    void shouldCheckMigrationDone() {
+        // given
+        var query = "show tables";
+        var sizeExpected = 1;
+        var tablesActual = new HashSet<>();
+        var tablesExpected = List.of(
+            "SUBJECTS"
+        );
 
+        // when
+        var rs = template.queryForRowSet(query);
+        while (rs.next()) {
+            tablesActual.add(rs.getString("TABLE_NAME"));
+        }
+        tablesActual.remove("flyway_schema_history");
+
+        // then
+        assertThat(tablesActual.size()).isEqualTo(sizeExpected);
+        assertThat(tablesActual).containsAll(tablesExpected);
     }
 }
