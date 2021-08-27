c70d8a199dc7ed60e00a228624e5e0311f2fb94e
Mishin Yura <mishin.inbox@gmail.com>
2021-08-27 20:02:25 +0300

    Amended PostgreSQL Init.sql. Closes #39

--- university/src/main/resources/db/migration/postgres/V1__Init.sql
+++ university/src/main/resources/db/migration/postgres/V1__Init.sql
@@ -1,5 +1,54 @@
 create table subjects
 (
-    id bigserial primary key,
+    id   bigserial primary key,
     name varchar(50) unique
+);
+
+create table groups
+(
+    id   bigserial primary key,
+    name varchar(50) unique
+);
+
+create table time
+(
+    id        bigserial primary key,
+    startTime time,
+    endTime   time
+);
+
+create table classrooms
+(
+    id   bigserial primary key,
+    name varchar(50) unique
+);
+
+create table roles
+(
+    id   bigserial primary key,
+    name varchar(50) unique
+);
+
+create table schedule
+(
+    id           bigserial primary key,
+    group_id     bigint references groups (id),
+    subject_id   bigint references subjects (id),
+    day          smallint,
+    time_id      bigint references time (id),
+    classroom_id bigint references classrooms (id),
+    role_id      bigint references roles (id)
+);
+
+create table users
+(
+    id   bigserial primary key,
+    name varchar(50)
+);
+
+create table user_role
+(
+    user_id bigint references users,
+    role_id bigint references roles,
+    primary key (user_id, role_id)
 );
