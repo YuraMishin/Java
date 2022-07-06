bd61de682d0f8badcdfea66e5b57df8557cbfed0
Mishin Yura <mishin.inbox@gmail.com>
2022-07-06 16:39:17 +0300

    Updated "is prime" solution. Closes #59

---
+++ core/src/main/java/isprime/SolutionV2.java
@@ -0,0 +1,31 @@
+package isprime;
+
+import java.util.function.IntPredicate;
+
+/**
+ * Class Solution.
+ * Implements entity Solution to check if the number is prime.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 06.07.2022
+ */
+public final class SolutionV2 implements IntPredicate {
+    /**
+     * @param n Value
+     * @return boolean
+     */
+    @Override
+    public boolean test(final int n) {
+        if (n == 1) {
+            return false;
+        }
+        int i = 2;
+        while (i < n) {
+            if (n % i == 0) {
+                return false;
+            }
+            i = i + 1;
+        }
+        return true;
+    }
+}
--- core/src/test/java/isprime/SolutionTest.java
+++ core/src/test/java/isprime/SolutionTest.java
@@ -13,6 +13,8 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class SolutionTest {
     private final Solution solution = new Solution();
+    private final SolutionV2 solutionV2 = new SolutionV2();
+    private final SolutionV3 solutionV3 = new SolutionV3();
 
     /**
      * Tests isPrime() method.
@@ -23,9 +25,13 @@ class SolutionTest {
 
         // when
         var result = solution.test(5);
+        var result2 = solutionV2.test(5);
+        var result3 = solutionV3.test(5);
 
         // then
         assertThat(result).isTrue();
+        assertThat(result2).isTrue();
+        assertThat(result3).isTrue();
     }
 
     /**
@@ -37,8 +43,12 @@ class SolutionTest {
 
         // when
         var result = solution.test(6);
+        var result2 = solutionV2.test(6);
+        var result3 = solutionV3.test(6);
 
         // then
         assertThat(result).isFalse();
+        assertThat(result2).isFalse();
+        assertThat(result3).isFalse();
     }
 }
