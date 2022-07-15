622476e16971aa605d2438fcad9e5dd3962de424
Mishin Yura <mishin.inbox@gmail.com>
2022-07-15 08:48:36 +0300

    Update factorial solution

--- core/src/main/java/factorial/FactorialLoop.java
+++ core/src/main/java/factorial/FactorialLoop.java
@@ -1,5 +1,7 @@
 package factorial;
 
+import java.math.BigInteger;
+
 /**
  * Class FactorialLoop.
  * Implements factorial calculation over loop.
@@ -18,6 +20,20 @@ public class FactorialLoop {
         int result = 1;
         for (int i = n; i > 1; i--) {
             result = result * i;
+        }
+        return result;
+    }
+
+    /**
+     * Method calculates factorial.
+     *
+     * @param n Number.
+     * @return Factorial.
+     */
+    public final BigInteger calcV2(final int n) {
+        var result = BigInteger.ONE;
+        for (int i = n; i > 1; i--) {
+            result = result.multiply(BigInteger.valueOf(i));
         }
         return result;
     }
